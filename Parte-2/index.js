c7b3786c739e4b7a7cb6c82189f390b0f2b22716
Pedro Gamez <gamezpedro1997@gmail.com>
2020-01-29 08:52:09 -0600

    Final Commit

--- Parte-2/index.js
+++ Parte-2/index.js
@@ -3,12 +3,44 @@ let bodyParser = require( 'body-parser' );
 let mongoose = require( 'mongoose' );
 let jsonParser = bodyParser.json();
 let { DATABASE_URL, PORT } = require( './config' );
+let{ bookmarkList } = require('./model');
+
 
 let app = express();
 
 let server;
 
 /* Tu código va aquí */
+app.put( '/api/bookmarks/:id',jsonParser, ( req, res ) => {
+	let id = req.body.id;
+	let id2 = req.params.id;
+	let title = req.body.title;
+	let description = req.body.description;
+	let url = req.body.url;
+
+	if(!id){
+		res.statusMessage = "Missing Field ID";
+		return res.status( 406 ).send();
+	}
+	else if(id !== id2){
+		res.statusMessage = "ID's do not Match";
+		return res.status( 409 ).json();
+	}
+	else if (!title && !description && !url){
+		res.statusMessage = "No changed Data";
+		return res.status( 406 ).json();
+	}
+	bookmarkList.put({ id : id }, req.body)
+       .then(bookmark => {
+           res.status(202).json(bookmark);
+       })
+       .catch(err => {
+           res.statusMessage = "Missing field in body";
+           return res.status(500).json({
+           });
+       });
+});
+
 
 function runServer( port, databaseUrl ){
 	return new Promise( (resolve, reject ) => {
--- Parte-2/model.js
+++ Parte-2/model.js
@@ -5,6 +5,34 @@ mongoose.Promise = global.Promise;
 
 /* Tu código va aquí */
 
-module.exports = {
-    
+let bookmarks = mongoose.Schema({
+    id : {type: uuid.v4()},
+    titulo : {type : String},
+    description : {type : String},
+    url : {type : String}
+});
+
+let bookmark = mongoose.model('bookmark', bookmarks);
+let bookmarkList = {
+
+    get : function(){
+		return bookmark.find()
+				.then( bookmark => {
+					return bookmark;
+				})
+				.catch( error => {
+					throw Error( error );
+				});
+    },
+    put : function(filer, updatedInfo){
+        return bookmark.updateOne(filer, updatedInfo)
+                .then( bookmark => {
+                    return bookmark;
+                })
+                .catch( err=> {
+                    throw Error(err);   
+                });
+    },
 };
+
+module.exports = {bookmarkList};
